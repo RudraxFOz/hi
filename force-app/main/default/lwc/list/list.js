2738803e369db6e1b8fe5c8802097620b7ca6e04
tuannguyenvn <tuannguyen.de@gmail.com>
2021-02-11 17:31:50 +0100

    try to filter card in list

--- force-app/main/default/lwc/list/list.js
+++ force-app/main/default/lwc/list/list.js
@@ -4,14 +4,27 @@ import getCardList from "@salesforce/apex/CardController.getCardList";
 export default class List extends LightningElement {
   @wire(getCardList) cards;
 
+  filteredCardsByType = [];
   _type = {};
 
   @api
   get type() {
-      return this._type;
+    return this._type;
   }
   set type(value) {
-      this._type = value;
+    this._type = value;
   }
 
+  filterCardsByType() {
+    if (this.cards) {
+      this.filteredCardsByType = this.cards.filter(
+        (card) => card.TypeId__c === this._type.Id
+      ); 
+    }
+    //Todo: add Error Panel
+  }
+
+  connectedCallback() {
+    this.filterCardsByType();
+  }
 }
